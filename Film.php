b46f494c9b726b15ddf2708ca0b5f269397c8428
FutRR <futterermaxime@gmail.com>
2024-02-02 10:54:30 +0100

    Linked Realisateur and Film

--- Film.php
+++ Film.php
@@ -6,13 +6,16 @@ class Film
     private DateTime $sortieFR;
     private int $duree;
     private string $synopsis;
+    private Realisateur $realisateur;
 
-    public function __construct(string $titre, string $sortieFR, int $duree, string $synopsis)
+    public function __construct(string $titre, string $sortieFR, int $duree, string $synopsis, Realisateur $realisateur)
     {
         $this->titre = $titre;
         $this->sortieFR = new DateTime($sortieFR);
         $this->duree = $duree;
         $this->synopsis = $synopsis;
+        $this->realisateur = $realisateur;
+        $this->realisateur->addFilms($this);
     }
 
     public function getTitre(): string
@@ -61,6 +64,17 @@ class Film
         $this->synopsis = $synopsis;
 
         return $this;
+    }
+
+    public function getDate()
+    {
+        $result = $this->sortieFR->format("d-m-Y");
+        return $result;
+    }
+
+    public function __toString()
+    {
+        return "$this->titre | $this->duree, sorti le " . $this->getDate() . " en France<br>";
     }
 
 }
--- Realisateur.php
+++ Realisateur.php
@@ -2,10 +2,29 @@
 
 class Realisateur extends Personne
 {
+    private array $films;
 
     public function __construct(string $nom, string $prenom, string $dateNaissance, string $sexe)
     {
         parent::__construct($nom, $prenom, $dateNaissance, $sexe);
+        $this->films = [];
+    }
+
+    public function getFilms()
+    {
+        return $this->films;
+    }
+
+    public function setFilms($films)
+    {
+        $this->films = $films;
+
+        return $this;
+    }
+
+    public function addFilms(Film $film)
+    {
+        $this->films[] = $film;
     }
 
 }
