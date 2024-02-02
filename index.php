bf2304da16e80f9668a527b553e2cf1deb51e71d
FutRR <futterermaxime@gmail.com>
2024-02-02 11:57:16 +0100

    Class Casting initialized

--- index.php
+++ index.php
@@ -4,21 +4,21 @@ spl_autoload_register(function ($class_name) {
     include $class_name . '.php';
 });
 
-$g1 = new Genre("Action");
-$g2 = new Genre("SF");
-$g3 = new Genre("Horreur");
+$action = new Genre("Action");
+$sf = new Genre("SF");
+$horreur = new Genre("Horreur");
 
-$r1 = new Realisateur("Eggers", "Robert", "07-07-1983", "M");
-$r2 = new Realisateur("Lucas", "George", "14-05-1944", "M");
+$eggers = new Realisateur("Eggers", "Robert", "07-07-1983", "M");
+$lucas = new Realisateur("Lucas", "George", "14-05-1944", "M");
 
-$f1 = new Film("The Northman", "11-05-2022", 137, $r1, $g1);
-$f2 = new Film("The Lighthouse", "18-12-2019", 110, $r1, $g3);
+$theNorthman = new Film("The Northman", "11-05-2022", 137, $eggers, $action);
+$theLighthouse = new Film("The Lighthouse", "18-12-2019", 110, $eggers, $horreur);
 
-$f3 = new Film("Star Wars - Episode IV : A New Hope", "19-10-1977", 121, $r2, $g2);
+$starWars4 = new Film("Star Wars - Episode IV : A New Hope", "19-10-1977", 121, $lucas, $sf);
 
 
-echo $r1->getInfos();
+echo $eggers->getInfos();
 
-echo $r2->getInfos();
+echo $lucas->getInfos();
 
-echo $g1->getInfos();
+echo $action->getInfos();
